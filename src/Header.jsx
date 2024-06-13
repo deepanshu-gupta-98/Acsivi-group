f429c0ffd2390b943a4bf144a0f45e2eaeebc49d
deepanshu-gupta-98 <gupta.deepanshu082@gmail.com>
2024-06-13 16:09:36 +0530

    second commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,10 +17,23 @@ function Header() {
     <div className="second">
       <h1 className="title3">Let's grow <br />together...</h1>
     </div>
-
-
-    <div>
+<div className='con'>
+    <div className='box'>
       <h1 className="third">Who are We?</h1>
+      <h4 className="des">Acsivi is a group of creative and tech-savvy individuals who <br />believe in the catalytic power of technology and the use of its <br /> knowledge to build a better experience for you.</h4>
+    </div>
+<div className='box2'>
+      <img src="https://img.freepik.com/free-photo/modern-equipped-computer-lab_23-2149241219.jpg?t=st=1718262772~exp=1718266372~hmac=19d544ee0c50876dd0ee1a6d0939efea26b4218f25b48c31a6ba40336ddd1216&w=900" alt="" className="pic2" />
+    </div>
+    </div>
+<div className='con'>
+<div className='box2'>
+      <img src="https://img.freepik.com/free-photo/modern-equipped-computer-lab_23-2149241219.jpg?t=st=1718262772~exp=1718266372~hmac=19d544ee0c50876dd0ee1a6d0939efea26b4218f25b48c31a6ba40336ddd1216&w=900" alt="" className="pic3" />
+    </div>
+    <div className='box'>
+      <h1 className="third">Who are We?</h1>
+      <h4 className="des">Acsivi is a group of creative and tech-savvy individuals who <br />believe in the catalytic power of technology and the use of its <br /> knowledge to build a better experience for you.</h4>
+    </div>
     </div>
    </>
   )
